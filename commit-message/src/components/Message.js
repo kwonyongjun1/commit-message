17c8ba7f70fa0aaeed5d1ac14d56fb8aee3585fa
subinJeong <wjdrnqls1004@gmail.com>
2023-12-01 00:37:18 +0900

    ✨Feat : message 컴포넌트 생성

---
+++ commit-message/src/components/Message.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Message() {
+    return(
+        <div>
+           Message 컴포넌트
+        </div>
+    );
+}
---
+++ commit-message/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Message from '../components/Message';
+import ResultList from '../components/ResultList';
+export default function MainPage() {
+    return(
+        <div>
+            <Message/>
+            <ResultList/>            
+        </div>
+    );
+}
