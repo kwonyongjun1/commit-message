f3edc7ed74301220112ebbd1f000673a6e07b6e8
kwonyongjun1 <dydwns18011@gmail.com>
2023-12-05 22:41:56 +0900

    ✨ Feat : Commit Message 내용 저장 기능

--- commit-message/src/components/LeftCommitMessage.js
+++ commit-message/src/components/LeftCommitMessage.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Message() {
+    const [auto, setAuto] = useState(true);
+    const [emoji, setEmoji] = useState("✨")
+    const [type, setType] = useState();
+    const [scope, setScope] = useState();
+    const [body, setBody] = useState();
+    const [footer, setFooter] = useState();
+
+    const onChangeType = (event) => {
+        setType(event.target.value);
+    };
+
+    const onChangeScope = (event) => {
+        setScope(event.target.value);
+    };
+
+    const onChangeBody = (event) => {
+        setBody(event.target.value);
+    };
+
+    const onChangeFooter = (event) => {
+        setFooter(event.target.value);
+    };
+
+    const onClickToggleAuto = () => {
+
+    };
+
+    // todo
+    const copyMessage = () => {
+        console.log(type);
+        console.log(scope);
+        console.log(body);
+        console.log(footer);
+    };
+
     return(
         <div>
-           Message 컴포넌트
+           <article>
+                <button onClick={onClickToggleAuto}>auto</button>
+                <header>
+                    <button id="emoji">{emoji}</button>
+                    <input type="text" id="type" placeholder="type" onChange={onChangeType}/>
+                    :
+                    <input type="text" id="scope" placeholder="scope" onChange={onChangeScope}/>
+                </header>    
+                
+                <textarea id="body" placeholder="body" onChange={onChangeBody}>
+                </textarea>
+
+                <textarea id="footer" placeholder="footer" onChange={onChangeFooter}>
+                </textarea>
+           </article>
+
+           <button onClick={copyMessage}>저장</button>
         </div>
     );
 }
