224d6f00dd4961ed10e1a879b46021124db2fbcb
kwonyongjun1 <dydwns18011@gmail.com>
2023-12-09 00:32:17 +0900

    ✨Feat : redux 적용

--- commit-message/src/index.js
+++ commit-message/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './services/data';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
