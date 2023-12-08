8fca7bb9f3a2915ed1df48627f576ec4927ad073
kwonyongjun1 <dydwns18011@gmail.com>
2023-12-08 23:11:39 +0900

    ✨ Feat : 토글 기능 추가

--- commit-message/src/components/LeftCommitMessage.js
+++ commit-message/src/components/LeftCommitMessage.js
@@ -32,9 +32,7 @@ export default function Message() {
     };
 
     const onClickToggleAuto = () => {
-        //let isAuto = auto;
         setAuto(!auto);
-        console.log(auto);
     };
 
     // todo
