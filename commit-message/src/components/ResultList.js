e100bfc0f514500632f693ee8d48cf16847b9ceb
kwonyongjun <dydwns18011@gmail.com>
2023-12-13 23:58:34 +0900

    ✨Feat : combineReducers 적용

--- commit-message/src/components/ResultList.js
+++ commit-message/src/components/ResultList.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSelector} from "react-redux";
 
 export default function ResultList() {
+
+    const type = useSelector(state => state.Message.type);
+    
     return(
         <div>
            ResultList 컴포넌트
+           { type }
         </div>
     );
 }
