a316625743259cabb04985bea4143659365dd69f
kwonyongjun <dydwns18011@gmail.com>
2023-12-18 22:32:45 +0900

    🚚 Rename : data.js 경로 변경

--- commit-message/src/index.js
+++ commit-message/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './services/data';
+import './database/data';
 import store from './redux/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
