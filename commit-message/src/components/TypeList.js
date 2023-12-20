4714e81332b41dca746a3474bc4ec1fee924abb5
kwonyongjun <dydwns18011@gmail.com>
2023-12-20 19:20:13 +0900

    ✨ Feat: typeList 표출

--- commit-message/src/components/TypeList.js
+++ commit-message/src/components/TypeList.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useSelector} from "react-redux";
-
+import { typeList} from "../database/data"
 export default function TypeList() {
     const type = useSelector(state => state.Message.type);
 
     return(
         <div>
            TypeList 컴포넌트
-           {type}
+           <br/>
+           검색한 내용 : {type}
+           {typeList.map((item) => (
+            <div key={item.name}>
+                <li>{item.name} {item.description}</li>
+            </div>  
+            ))}
         </div>
     );
 }
