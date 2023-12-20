9dc28c421cf3d8a4a810fda4e7669724c0843a72
kwonyongjun <dydwns18011@gmail.com>
2023-12-20 19:21:04 +0900

    ♻️ Refactor: test 코드 삭제

--- commit-message/src/components/EmojiList.js
+++ commit-message/src/components/EmojiList.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { emojiList} from '../database/data';
 export default function EmojiList() {
-
-    const test = () =>{
-        console.log(emojiList);
-    };
+ 
     return(
         <div>
            emojiList 컴포넌트
            {emojiList.map((item) => (
-          <div key={item.id}>
-              <li>{item.id} {item.emoji}</li>
-          </div>  
-        ))}
+            <div key={item.id}>
+                <li>{item.id} {item.emoji}</li>
+            </div>  
+            ))}
         </div>
     );
 } 
