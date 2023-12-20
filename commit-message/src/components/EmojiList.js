faf569f08dc29bb38581c1053bf4395638ce56e5
kwonyongjun <dydwns18011@gmail.com>
2023-12-20 19:17:58 +0900

    ✨ Feat: emojiList 표출

--- commit-message/src/components/EmojiList.js
+++ commit-message/src/components/EmojiList.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import "../database/data";
+import { emojiList} from '../database/data';
 export default function EmojiList() {
 
+    const test = () =>{
+        console.log(emojiList);
+    };
     return(
         <div>
            emojiList 컴포넌트
+           {emojiList.map((item) => (
+          <div key={item.id}>
+              <li>{item.id} {item.emoji}</li>
+          </div>  
+        ))}
         </div>
     );
-}
+} 
