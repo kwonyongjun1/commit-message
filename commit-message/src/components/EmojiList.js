4e8c04dc32725185dc7dbff932e0515f4416ad81
kwonyongjun <dydwns18011@gmail.com>
2023-12-21 21:07:58 +0900

    ✨ Feat: emoji 설정 기능 추가

--- commit-message/src/components/EmojiList.js
+++ commit-message/src/components/EmojiList.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
 import { emojiList} from '../database/data';
+import { findEmojiFromId} from "../utils"
+import { setEmoji } from '../redux/actions/Message';
 export default function EmojiList() {
- 
+
+    const dispatch = useDispatch();
+    const onClickEmoji = (id) => {
+        let emoji = findEmojiFromId(id);
+        dispatch(setEmoji(emoji.emoji));
+    };
+
+    
     return(
         <div>
            emojiList 컴포넌트
            {emojiList.map((item) => (
-            <div key={item.id}>
+            <div key={item.id} onClick={()=>{onClickEmoji(item.id)}}>
                 <li>{item.id} {item.emoji}</li>
             </div>  
             ))}
