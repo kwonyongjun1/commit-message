bfe1ef9c81e6061b1bedf9acbc81d1249fdb6d65
kwonyongjun <dydwns18011@gmail.com>
2024-01-04 00:31:51 +0900

    ️♻ Refactor: 코드 정리

--- commit-message/src/components/ContentsList.js
+++ commit-message/src/components/ContentsList.js
@@ -5,10 +5,10 @@ import EmojiList from "./EmojiList";
 import TypeList from "./TypeList";
 export default function ContentsList() {
 
-    const state = useSelector(state => state.Message.currentState)
+    const state = useSelector(state => state.Message.currentState);
+
     return(
         <div>
-           ContentsList 컴포넌트
            { state === STATE.EMOJI_MODE && <EmojiList/>}
            { state === STATE.TYPE_MODE && <TypeList/> }
         </div>
--- commit-message/src/pages/Main.js
+++ commit-message/src/pages/Main.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import LeftCommitMessage from '../components/LeftCommitMessage';
 import ContentsList from '../components/ContentsList';
+import EmojiList from '../components/EmojiList';
 import SlideList from '../components/SlideList'
 export default function MainPage() {
     return(
-        <div>
+        <div className='mainBody'>
             <LeftCommitMessage/>
-            <ContentsList/>
+            <EmojiList/>
         </div>
     );
 }
