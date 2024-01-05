ac37abadc7e4bcee115efbe1fdf02d953079e371
kwonyongjun <dydwns18011@gmail.com>
2024-01-05 22:51:52 +0900

    🔥Remove: ContentList 컴포넌트 삭제

--- commit-message/src/components/ContentsList.js
+++ commit-message/src/components/ContentsList.js
@@ -4,13 +4,33 @@ import {STATE} from "../constants";
 import EmojiList from "./EmojiList";
 import TypeList from "./TypeList";
 export default function ContentsList() {
+    var 탭UI = { 
+        info : <p>상품정보</p>,
+        shipping : <p>배송관련</p>,
+        refund : <p>환불약관</p>
+      }
+      
+      function Component() {
+        var 현재상태 = 'info';
+        return (
+          <div>
+            {
+              탭UI[현재상태]
+            }
+          </div>
+        )
+      } 
 
     const state = useSelector(state => state.Message.currentState);
-
     return(
         <div>
-           { state === STATE.EMOJI_MODE && <EmojiList/>}
-           { state === STATE.TYPE_MODE && <TypeList/> }
+            {
+                {
+                    STATE.EMOJI_MODE : <EmojiList/>,
+                    STATE.TYPE_MODE : <TypeList/>
+                }[state]
+            }
+          
         </div>
     );
 }
