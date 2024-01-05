72bf8032138178e50486f60aaf377f4ec9665799
kwonyongjun <dydwns18011@gmail.com>
2024-01-05 23:20:03 +0900

    ✨Feat: typelist filter 기능 추가

--- commit-message/src/components/TypeList.js
+++ commit-message/src/components/TypeList.js
@@ -2,18 +2,24 @@ import React from 'react'
 import { useSelector} from "react-redux";
 import { typeList} from "../database/data"
 export default function TypeList() {
-    const type = useSelector(state => state.Message.type);
+    const selectedType = useSelector(state => state.Message.type);
 
     return(
-        <div>
-           TypeList 컴포넌트
-           <br/>
-           검색한 내용 : {type}
-           {typeList.map((item) => (
-            <div key={item.name}>
-                <li>{item.name} {item.description}</li>
-            </div>  
-            ))}
+        <div className='middle-wrap'>
+            <div>
+            {typeList.map((item) => (
+                    item.name.includes(selectedType)  && (
+                    <dl className="type-box" key={item.name}>
+                        <dt>
+                            {item.name}
+                        </dt>
+                        <dd>
+                            {item.description}
+                        </dd>
+                    </dl>
+                    ) 
+                ))}
+            </div>
         </div>
     );
 }
