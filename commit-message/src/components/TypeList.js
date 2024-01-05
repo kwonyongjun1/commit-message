a668f6f64c16baf9a7083ca140360db4f9d3b454
kwonyongjun <dydwns18011@gmail.com>
2024-01-06 01:46:56 +0900

    ✨ Feat: emoji auto 기능 추가

--- commit-message/src/components/TypeList.js
+++ commit-message/src/components/TypeList.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { useSelector} from "react-redux";
 import { typeList} from "../database/data"
 import { useDispatch } from 'react-redux';
-import { setType } from "../redux/actions/Message"
+import { setType, setEmoji } from "../redux/actions/Message";
+import { findEmojiFromRelType } from "../utils";
 export default function TypeList() {
-    
+
     const dispatch = useDispatch();
     const selectedType = useSelector(state => state.Message.type);
+    const autoMode = useSelector(state => state.Message.autoMode);
 
     const onClickType = (type) =>{
         dispatch(setType(type.name));
+        if(autoMode){
+            let emoji = findEmojiFromRelType(type.name);
+            if(emoji.emoji){
+                dispatch(setEmoji(emoji.emoji));
+            }
+        }
     };
 
     return(
