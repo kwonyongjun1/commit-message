351a0a53d710385eca1c6eb4ba1b5a16e4ec5e81
kwonyongjun <dydwns18011@gmail.com>
2024-01-06 01:06:15 +0900

    🎨 Style: 불필요한 소스 삭제

--- commit-message/src/components/EmojiList.js
+++ commit-message/src/components/EmojiList.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { emojiList} from '../database/data';
-import { findEmojiFromId} from "../utils"
+import { useDispatch } from "react-redux";
+import { emojiList } from '../database/data';
+import { findEmojiFromId } from "../utils"
 import { setEmoji } from '../redux/actions/Message';
 export default function EmojiList() {
 
@@ -14,7 +14,7 @@ export default function EmojiList() {
     
     return(
         <div className = "middle-wrap">
-            <div className="grid-container">
+            <div className="grid-container-emoji">
             {emojiList.map((item) => (
                 <dl className="grid-item"  key={item.id} onClick={()=>{onClickEmoji(item.id)}}>
                     <dt className="emoji">
