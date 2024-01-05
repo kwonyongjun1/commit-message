26741e6a1c183e1a6f9cbb419f5b1a53b3fa8ce8
kwonyongjun <dydwns18011@gmail.com>
2024-01-06 00:58:39 +0900

    ✨ Feat: commit message copy 기능 추가

--- commit-message/src/utils/copyMessage.js
+++ commit-message/src/utils/copyMessage.js
@@ -1,7 +1,7 @@
 export const copyMessage = (message) => {
 
     let { emoji, type, scope, shortSummary, body, footer } = message;
-    const header = !!scope && `${emoji} ${type + (scope)}: ${shortSummary}` || `${emoji} ${type}: ${shortSummary}`;
+    const header = !!scope && `${emoji} ${type}(${scope}): ${shortSummary}` || `${emoji} ${type}: ${shortSummary}`;
     body = !!body ? `\n\n${body}` : "";
     footer = !!footer ? `\n\n${footer}` : "";
     const text = header + body + footer;
