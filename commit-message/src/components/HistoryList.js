c2600dc6cbf51d3af2dfba429055a0dab0936249
kwonyongjun <dydwns18011@gmail.com>
2024-01-08 23:49:48 +0900

    ✨ Feat: history list 추가

--- commit-message/src/components/HistoryList.js
+++ commit-message/src/components/HistoryList.js
@@ -1,151 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function HistoryList(){
-    const onClickMessage = () =>{
-        
+
+    useEffect(() => {
+        let message = localStorage.getItem("preMessage")
+        setHistoryMessage(JSON.parse(message));
+    }, []);
+
+    const [historyMessage, setHistoryMessage] = useState([]);
+   
+    const onClickMessage = (message) =>{
+        setMessage(message);
     }
 
-    const setMessage = () =>{
-
+    const onClickDelete = () =>{
+        // TODO localstorage delete 
     }
+    const setMessage = (message) =>{
+        // TODO setMessage
+    }
+    
     return(
         <div className = "middle-wrap">
             <div className="grid-container-history">
-                <div className="grid-item" onClick={onClickMessage}>
-                    <div className='commit-message'>
-                        <header>
-                            <button id="emoji" disabled>emoji</button>
-                            <input className="type" type="text" placeholder="type" readOnly="true"/>
-                            <input className="scope" type="text" placeholder="scope" readOnly="true"/>
-                            :
-                            <input className="shortSummary" type="text" placeholder="make log easier (#123)" readOnly="true"/>
-                        </header>
-                        <div className = "body">
-                            <textarea className="commitBody" placeholder="body"></textarea>
-                            <textarea className="commitFooter" placeholder="footer"></textarea>
+                {historyMessage.map((message)=> (
+                    <div className="grid-item" onClick={()=>{onClickMessage(message)}}>
+                        <div className='commit-message'>
+                            <header onClick={(event)=>{event.stopPropagation()}}>
+                                <button id="emoji" disabled>{item.emoji}</button>
+                                <input className="type" type="text" placeholder="type" value={item.type}/>
+                                <input className="scope" type="text" placeholder="scope" value={item.scope}/>
+                                :
+                                <input className="shortSummary" type="text" placeholder="make log easier (#123)" value={item.shortSummary}/>
+                                <button onClick={onClickDelete}>삭제</button>
+                            </header>
+                            <div className = "body">
+                                <div className="commitBody historyBox" placeholder="body">{item.body}</div>
+                                <div className="commitFooter historyBox" placeholder="footer">{item.footer}</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="grid-item">
-                    <div className='commit-message'>
-                        <header>
-                            <button id="emoji">emoji</button>
-                            <input className="type" type="text" placeholder="type"/>
-                            <input className="scope" type="text" placeholder="scope" />
-                            :
-                            <input className="shortSummary" type="text" placeholder="make log easier (#123)"/>
-                        </header>
-                        <div className = "body">
-                            <textarea className="commitBody" placeholder="body"></textarea>
-                            <textarea className="commitFooter" placeholder="footer"></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid-item">
-                    <div className='commit-message'>
-                        <header>
-                            <button id="emoji">emoji</button>
-                            <input className="type" type="text" placeholder="type"/>
-                            <input className="scope" type="text" placeholder="scope" />
-                            :
-                            <input className="shortSummary" type="text" placeholder="make log easier (#123)"/>
-                        </header>
-                        <div className = "body">
-                            <textarea className="commitBody" placeholder="body"></textarea>
-                            <textarea className="commitFooter" placeholder="footer"></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid-item">
-                    <div className='commit-message'>
-                        <header>
-                            <button id="emoji">emoji</button>
-                            <input className="type" type="text" placeholder="type"/>
-                            <input className="scope" type="text" placeholder="scope" />
-                            :
-                            <input className="shortSummary" type="text" placeholder="make log easier (#123)"/>
-                        </header>
-                        <div className = "body">
-                            <textarea className="commitBody" placeholder="body"></textarea>
-                            <textarea className="commitFooter" placeholder="footer"></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid-item">
-                    <div className='commit-message'>
-                        <header>
-                            <button id="emoji">emoji</button>
-                            <input className="type" type="text" placeholder="type"/>
-                            <input className="scope" type="text" placeholder="scope" />
-                            :
-                            <input className="shortSummary" type="text" placeholder="make log easier (#123)"/>
-                        </header>
-                        <div className = "body">
-                            <textarea className="commitBody" placeholder="body"></textarea>
-                            <textarea className="commitFooter" placeholder="footer"></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid-item">
-                    <div className='commit-message'>
-                        <header>
-                            <button id="emoji">emoji</button>
-                            <input className="type" type="text" placeholder="type"/>
-                            <input className="scope" type="text" placeholder="scope" />
-                            :
-                            <input className="shortSummary" type="text" placeholder="make log easier (#123)"/>
-                        </header>
-                        <div className = "body">
-                            <textarea className="commitBody" placeholder="body"></textarea>
-                            <textarea className="commitFooter" placeholder="footer"></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid-item">
-                    <div className='commit-message'>
-                        <header>
-                            <button id="emoji">emoji</button>
-                            <input className="type" type="text" placeholder="type"/>
-                            <input className="scope" type="text" placeholder="scope" />
-                            :
-                            <input className="shortSummary" type="text" placeholder="make log easier (#123)"/>
-                        </header>
-                        <div className = "body">
-                            <textarea className="commitBody" placeholder="body"></textarea>
-                            <textarea className="commitFooter" placeholder="footer"></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid-item">
-                    <div className='commit-message'>
-                        <header>
-                            <button id="emoji">emoji</button>
-                            <input className="type" type="text" placeholder="type"/>
-                            <input className="scope" type="text" placeholder="scope" />
-                            :
-                            <input className="shortSummary" type="text" placeholder="make log easier (#123)"/>
-                        </header>
-                        <div className = "body">
-                            <textarea className="commitBody" placeholder="body"></textarea>
-                            <textarea className="commitFooter" placeholder="footer"></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid-item">
-                    <div className='commit-message'>
-                        <header>
-                            <button id="emoji">emoji</button>
-                            <input className="type" type="text" placeholder="type"/>
-                            <input className="scope" type="text" placeholder="scope" />
-                            :
-                            <input className="shortSummary" type="text" placeholder="make log easier (#123)"/>
-                        </header>
-                        <div className = "body">
-                            <textarea className="commitBody" placeholder="body"></textarea>
-                            <textarea className="commitFooter" placeholder="footer"></textarea>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     );
