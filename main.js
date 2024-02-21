1e26e3d011bccb382561cc215ba9f8d4934a2c96
kwonyongjun <dydwns18011@gmail.com>
2024-02-21 16:40:00 +0900

    ✨ Feat: Electron 적용

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1800,
+    height: 1000,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false
+    }
+  });
+
+  // win.loadFile(path.join(__dirname, 'public/index.html'));
+  win.loadURL(path.join('https://kwonyongjun1.github.io/commit-message/'));
+
+  if (process.platform !== 'darwin') {
+    win.removeMenu();
+  }
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
